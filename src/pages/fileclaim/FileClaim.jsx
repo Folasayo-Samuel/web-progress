81a97b97f53c87e007f460d71942c9ef0287a138
Folasayo Samuel <folasayosamuel@gmail.com>
2021-11-10 08:01:32 +0100

    update

--- src/pages/fileclaim/FileClaim.jsx
+++ src/pages/fileclaim/FileClaim.jsx
@@ -6,8 +6,8 @@ const FileClaim = () => {
     return (
         <div className="container">
             <div className="row">
-                <div className="col">
-                    <img src= { Speaker } alt="File Claim Speaker" />
+                <div className="col-sm">
+                    <img src= { Speaker } alt="File Claim Speaker" style={{width: '100%'}} />
                     <p className="pt-5"> Download Claim Form <span className="co">here</span></p>
                 </div>
                 <div className="col">
